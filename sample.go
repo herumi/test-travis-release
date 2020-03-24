b5247d7d95e34a7221e61dda545b34b956b906da
MITSUNARI Shigeo <herumi@nifty.com>
2020-03-24 17:31:17 +0900

    test2

--- sample.go
+++ sample.go
@@ -2,7 +2,7 @@ package main
 /*
 #include <stdio.h>
 #include <stdlib.h>
-void put(const char s[][3], size_t n)
+void put(const unsigned char s[][3], size_t n)
 {
 	for (size_t i = 0; i < n; i++) {
 		printf("%zd %c%c%c\n", i, s[i][0], s[i][1], s[i][2]);
@@ -12,7 +12,11 @@ void put(const char s[][3], size_t n)
 import "C"
 import "unsafe"
 
+func callPut(s []byte) {
+	C.put((*[3]C.uchar)(unsafe.Pointer(&s[0])), (C.size_t)(len(s) / 3))
+}
+
 func main() {
 	s := ([]byte)("abc012XYZQQQ");
-	C.put((*[3]C.char)(unsafe.Pointer(&s[0])), (C.size_t)(len(s) / 3))
+	callPut(s)
 }
