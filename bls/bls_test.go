3ffeae7cefd4819691341409a8186e9d95c94453
MITSUNARI Shigeo <herumi@nifty.com>
2020-03-27 16:54:17 +0900

    _

--- bls/bls_test.go
+++ bls/bls_test.go
@@ -7,19 +7,9 @@ import (
 func testVerifyHashDomain(t *testing.T) {
 	const S = 40
 	const N = 10
-	secs := make([]SecretKey, N)
 	pubs := make([]PublicKey, N)
-	sigs := make([]Sign, N)
 	hds := make([]byte, S*N)
-	for i := 0; i < N; i++ {
-		hds[i*S] = byte(i)
-		hd := hds[S*i : S*(i+1)]
-		secs[i].SetByCSPRNG()
-		pubs[i] = *secs[i].GetPublicKey()
-		sigs[i] = *secs[i].SignHashWithDomain(hd)
-	}
 	var sig Sign
-	sig.Aggregate(sigs)
 	if !sig.VerifyAggregateHashWithDomain(pubs, hds) {
 		t.Fatalf("bad VerifyAggregateHashWithDomain")
 	}
