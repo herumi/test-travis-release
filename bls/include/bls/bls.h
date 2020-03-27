8a14185608da4cffd8df05997adcc035605dae7f
MITSUNARI Shigeo <herumi@nifty.com>
2020-03-27 17:33:02 +0900

    _

--- bls/include/bls/bls.h
+++ bls/include/bls/bls.h
@@ -1,14 +1,6 @@
 #pragma once
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 #include <stdlib.h>
-typedef size_t mclSize;
 
-int blsVerifyAggregatedHashWithDomain(const void *aggSig, const void *pubVec, const unsigned char hashWithDomain[][40], size_t n);
+int blsFunc(const void *aggSig, const void *pubVec, const unsigned char hashWithDomain[][40], size_t n);
 
-#ifdef __cplusplus
-}
-#endif
