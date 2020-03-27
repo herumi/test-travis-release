6495d4df7e0007098f97be386764e24e1d759bd5
MITSUNARI Shigeo <herumi@nifty.com>
2020-03-27 18:07:01 +0900

    in

--- bls/bls.go
+++ bls/bls.go
@@ -5,7 +5,7 @@ package bls
 #cgo LDFLAGS:-lbls
 #cgo linux,amd64 LDFLAGS:-L${SRCDIR}/lib/linux/amd64
 #cgo darwin,amd64 LDFLAGS:-L${SRCDIR}/lib/darwin/amd64
-#include <bls/bls.h>
+void blsFunc(const char buf[][8]);
 */
 import "C"
 import (
