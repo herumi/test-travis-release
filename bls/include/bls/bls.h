4ee2a29a1315cb64ffc2b716249f30f5f1f2f348
MITSUNARI Shigeo <herumi@nifty.com>
2020-03-27 17:00:47 +0900

    _

--- bls/include/bls/bls.h
+++ bls/include/bls/bls.h
@@ -42,13 +42,6 @@ typedef struct {
 	mclBnG2 v;
 } blsSignature;
 
-
-int blsInit(int curve, int compiledTimeVar);
-int blsSecretKeySetByCSPRNG(blsSecretKey *sec);
-void blsGetPublicKey(blsPublicKey *pub, const blsSecretKey *sec);
-void blsAggregateSignature(blsSignature *aggSig, const blsSignature *sigVec, mclSize n);
-
-int blsSignHashWithDomain(blsSignature *sig, const blsSecretKey *sec, const unsigned char hashWithDomain[40]);
 int blsVerifyAggregatedHashWithDomain(const blsSignature *aggSig, const blsPublicKey *pubVec, const unsigned char hashWithDomain[][40], mclSize n);
 
 #ifdef __cplusplus
